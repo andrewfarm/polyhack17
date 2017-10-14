d061236c191a915817e6ca4a4083f5b9b5cff006
andrewfarm <Andrew.Farm@tufts.edu>
2017-10-14 00:34:29 -0400

    Sort classes by # of sections

--- saje.js
+++ saje.js
@@ -1,4 +1,14 @@
 console.log("classes (long form)", classesLong);
 console.log("classes (short form)", mutate(classesLong));
 
+var compare = function(a, b) {
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+};
 
+var compareNumSections = function(class1, class2) {
+        return compare(class1.sections.length, class2.sections.length);
+};
+
+console.log("sorted classes", mutate(classesLong).sort(compareNumSections));
